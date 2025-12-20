22ec8ed3ec2e662b923fae749578d718a3f331dd
Lucas Vieira <vieiralucas4@gmail.com>
2025-12-19 23:19:28 -0300

    2025 day 1 part 2

--- 2025/src/day1.zig
+++ 2025/src/day1.zig
@@ -38,6 +38,34 @@ pub fn part1(allocator: std.mem.Allocator, input: []const u8) ![]const u8 {
 }
 
 pub fn part2(allocator: std.mem.Allocator, input: []const u8) ![]const u8 {
-    _ = input;
-    return std.fmt.allocPrint(allocator, "TODO", .{});
+    var lines = std.mem.splitSequence(u8, input, "\n");
+    var pos: i32 = 50;
+    var result: u32 = 0;
+    while (lines.next()) |line| {
+        if (line.len < 1) {
+            continue;
+        }
+
+        const d = line[0];
+        if (d != 'L' and d != 'R') {
+            return error.LineInvalidDirection;
+        }
+
+        const step_str = line[1..];
+        const step = try std.fmt.parseInt(u32, step_str, 10);
+        for (0..step) |_| {
+            pos += if (d == 'L') -1 else 1;
+            if (pos == 100) {
+                pos = 0;
+            } else if (pos == -1) {
+                pos = 99;
+            }
+
+            if (pos == 0) {
+                result += 1;
+            }
+        }
+    }
+
+    return std.fmt.allocPrint(allocator, "{}", .{result});
 }
